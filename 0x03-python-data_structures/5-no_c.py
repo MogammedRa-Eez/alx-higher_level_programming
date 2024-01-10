62feae3856aa1caab07184ebba0e8fe6c1ae6681
MogammedRa-Eez <zardad2603@gmail.com>
2024-01-10 05:00:14 -0800

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my-string):
     new_string = ""
     for letter in my_string:
-        if letter is not "c" and letter is not "C":
+        if letter is not 'c' and letter is not 'C':
             new_string += letter
             return new_string
