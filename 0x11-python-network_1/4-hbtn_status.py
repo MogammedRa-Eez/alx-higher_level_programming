21aa429dafc4405f4ed483312ffe0c8018c4d723
MogammedRa-Eez <zardad2603@gmail.com>
2024-04-26 19:10:15 -0700

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
 """
-Script that fetches https://intranet.hbtn.io/status
-"""
-import requests
+Script that takes in a URL, sends a request to the URL and displays the
+body of the response (decoded in utf-8).
 
-if __name__ == '__main__':
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    text = r.text
-    print("Body response:")
-    print("\t- type: {}".format(type(text)))
-    print("\t- content: {}".format(text))
+Usage: ./3-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+from sys import argv
+from urllib.request import Request, urlopen
+from urllib.error import HTTPError
+
+
+if __name__ == "__main__":
+    url = argv[0]
+    req = Request(url)
+
+    try:
+        with urlopen(req) as response:
+            print(response.read().decode("ascii"))
+    except HTTPError as e:
+        print("Error code: {}".format(e.code))
