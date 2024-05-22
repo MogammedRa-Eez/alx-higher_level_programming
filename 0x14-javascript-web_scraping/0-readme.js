349220a0ed81e40ae344593cdc8845f1bae76a83
MogammedRa-Eez <zardad2603@gmail.com>
2024-05-22 16:55:28 -0700

    task 1

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,5 @@
 #!/usr/bin/node
-//  script that reads and prints the content of a file.
-
-const filePath = process.argv[2];
 const fs = require('fs');
-fs.readFile(filePath, 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(data);
-  }
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
